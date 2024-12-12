c116f99ed540d84de5f012f8e1d5431cfac165ca
Charday Neal <charday.neal@gmail.com>
2024-12-12 12:42:28 -0800

    Lift state to App component, add toggle feature and delete feature

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,35 @@ const TASKS = [
 
 const App = () => {
   const [taskData, setTaskData] = useState(TASKS);
-  const toggleCompleteTask = (id) => {}
+
+  const toggleCompleteTask = (id) => {
+    setTaskData(taskData => taskData.map(task => {
+      if (task.id === id) {
+        return {...task, isComplete: !task.isComplete};
+      } else {
+        return task;
+      }
+    }));
+  };
+
+  const removeTask = (id) => {
+    setTaskData(taskData => taskData.filter(task => {
+      return task.id !== id;
+    }));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>
+          <TaskList
+            tasks={taskData}
+            onTaskToggle={toggleCompleteTask}
+            onRemoveTask={removeTask}/>
+        </div>
       </main>
     </div>
   );

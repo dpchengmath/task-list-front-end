a3263dfab46d640c5510cb1a8d17c1b7936a928a
DC <denise.p.cheng@gmail.com>
2024-12-20 20:01:33 -0500

    Refactors to deployed version.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/task-list-front-end',
   plugins: [react()],
   test: {
     reporters: ['verbose'],
